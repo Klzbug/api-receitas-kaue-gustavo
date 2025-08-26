1d9f0010572eb51cc7edfeadb6c42b0552d7a056
Kauê Lázaro <lazarok667@gmail.com>
2025-08-26 23:41:58 +0000

    receitas

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ receitas = [
         'igredientes' : ['3 ovos', '6 colheres de açúcar','...'],
         'utensílios' : ['tijela', 'forma'],
         'modo de preparo' : '...'
+    },
+    {
+        'nome': 'Salada de fruta',
+        'igredientes' : ['maça', 'banana','uva','...'],
+        'utensílios' : ['tijela'],
+        'modo de preparo' : 'fatiar bem todas as frutas e misturalas'
     }
 ]
