907b0f9fda06e0e5e3a04436f05066048ba31495
Kauê Lázaro <lazarok667@gmail.com>
2025-08-26 23:36:47 +0000

    novo api

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+receitas = [
+    {
+        'nome': 'brownie',
+        'igredientes' : ['3 ovos', '6 colheres de açúcar','...'],
+        'utensílios' : ['tijela', 'forma'],
+        'modo de preparo' : '...'
+    },
+    {
+        'nome': 'torta',
+        'igredientes' : ['3 ovos', '6 colheres de açúcar','...'],
+        'utensílios' : ['tijela', 'forma'],
+        'modo de preparo' : '...'
+    }
+]
