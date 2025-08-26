988e50e0d6ef5190d605798aa9528dfe31d0a7d6
santozzx <gustavo.santos12@aluno.ifce.edu.br>
2025-08-26 23:57:28 +0000

    vasco

---
+++ main.py
@@ -0,0 +1,14 @@
+receitas = [
+    {
+        'nome': 'cuzcuz',
+        'ingrediente' : ['2 xícaras de flocão de milho', '1 xícara de água', '1/2 colher de chá de sal', '1 colher de sopa de manteiga (opcional)'],
+        'utensílios' : ['Tigela', 'Colher', 'Cuzcuzeira', 'Panela'],
+        'modo de preparo' :'Em uma tigela, misture o flocão de milho com o sal; Acrescente a água aos poucos, mexendo até que toda a farinha esteja úmida. Deixe descansar por 10 minutos para hidratar bem; Coloque a massa hidratada na cuscuzeira sem apertar muito; Leve ao fogo médio por cerca de 10 minutos ou até sentir o cheiro de milho cozido; Desenforme com cuidado, coloque manteiga por cima e sirva quente.'
+    },
+    {
+        
+    },
+    {
+
+    }
+]
