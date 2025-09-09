fca5141e878a4721a0a7d15e40de325db798cf89
Kauê Lázaro <lazarok667@gmail.com>
2025-09-09 11:32:33 +0000

    new

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 app = FastAPI(title='API do Kaué e do Gustavo')
 
-class Receita(BaseModel):
+
+class CreateReceita(BaseModel):
     nome: str
     ingredientes: List[str]
     modo_de_preparo: str
 
-'''
-receitas = [
-    {
-        'nome': 'Brownie',
-        'ingredientes': [
-            '3 ovos',
-            '6 colheres de açúcar',
-            '1/2 xícara (chá) de chocolate em pó',
-            '100g de manteiga',
-            '1 xícara (chá) de farinha de trigo'
-        ],
-        'utensílios': [
-            'Tigela',
-            'Forma',
-            'Colher de pau',
-            'Forno'
-        ],
-        'modo_de_preparo': 'Misture todos os ingredientes em uma tigela, coloque a massa em uma forma untada e leve ao forno pré-aquecido a 180°C por cerca de 30 minutos.'
-    },
-]
-'''
+class Receita(CreateReceita):
+    id: int
+
 
 receitas: List[Receita] = []
+contador_id = 1
+
 
 @app.get("/receitas")
 def get_todas_receitas():
@@ -42,37 +26,46 @@ def get_todas_receitas():
 @app.get("/receitas/{nome_receita}")
 def get_receitas_por_nome(nome_receita: str):
     for receita in receitas:
-        if receita.nome == nome_receita():
+        if receita.nome.lower() == nome_receita.lower():
             return receita
     return {"erro": "Receita não encontrada"}
 
 
 @app.post("/receitas")
-def create_receita(dados: Receita):
-    nova_receita = dados
+def create_receita(dados: CreateReceita):
+    global contador_id
+
+    nova_receita = Receita(
+        id=contador_id,
+        nome=dados.nome,
+        ingredientes=dados.ingredientes,
+        modo_de_preparo=dados.modo_de_preparo
+    )
 
     receitas.append(nova_receita)
+    contador_id += 1
 
     return nova_receita
 
-@app.put("/receitas{id}")
+
+@app.put("/receitas/{id}")
 def update_receita(id: int, dados: CreateReceita):
     for i in range(len(receitas)):
         if receitas[i].id == id:
-            receitas_atualizada = Receita (
+            receita_atualizada = Receita(
                 id=id,
                 nome=dados.nome,
-                igredientes=dados.igredientes,
+                ingredientes=dados.ingredientes,
                 modo_de_preparo=dados.modo_de_preparo,
             )
+            receitas[i] = receita_atualizada
+            return receita_atualizada
 
-        receitas[i] = (receita_atualizada)
-        return receita_atualizada
+    return {"erro": "Receita não encontrada"}
 
 
-def buscar_receita(id_receita):
-    for i in range(len(receitas)):
-        if receitas[i]["id"] == id_receita:
-            return receitas[i]
-    
+def buscar_receita(id_receita: int) -> dict:
+    for receita in receitas:
+        if receita.id == id_receita:
+            return receita
     return {"mensagem": "Receita não encontrada"}
